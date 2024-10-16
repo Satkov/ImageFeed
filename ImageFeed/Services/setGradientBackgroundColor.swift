8dbac1ee883cdf2e4cd6e6a0e7dc203f06bd6190
Satkov <g.satkov@mail.ru>
2024-10-16 20:26:55 +0300

    lint

--- ImageFeed/Services/setGradientBackgroundColor.swift
+++ ImageFeed/Services/setGradientBackgroundColor.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 func setGradientBackgroundColor(for view: UIView) {
-    let colorTop =  UIColor(red: 0/255.0, green: 149/255.0, blue: 150/255.0, alpha: 1.0).cgColor
+    let colorTop =  UIColor(red: 255/255.0, green: 149/255.0, blue: 150/255.0, alpha: 1.0).cgColor
     let colorBottom = UIColor(red: 58/255.0, green: 94/255.0, blue: 255/255.0, alpha: 1.0).cgColor
 
     let gradientLayer = CAGradientLayer()
