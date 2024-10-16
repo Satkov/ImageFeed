8a5f62a3c8ca03bee2255cbbbbf577a9736e0b01
Satkov <g.satkov@mail.ru>
2024-10-16 21:50:03 +0300

    сверстал часть страницы профиля

---
+++ ImageFeed/ProfilePage/ProfilePageViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ProfilePageViewController: UIViewController {
+    @IBOutlet weak var exitButton: UIButton!
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var userTagLabel: UILabel!
+    
+    override func viewDidLoad() {
+        
+    }
+}
